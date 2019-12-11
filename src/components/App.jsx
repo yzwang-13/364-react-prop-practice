1e18a2bf3a2c310e7bb0923ab9cebbe55d6ca903
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-11 22:26:52 +1100

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,40 @@
 import React from "react";
+import { Card } from "./Card";
+import { contacts } from "../contacts";
+
+// function AllCards() {
+//   var allCards = null;
+//   console.log(contacts);
+//   contacts.forEach(function(e) {
+//     allCards += (
+//       <Card name={e.name} image={e.imgURL} tel={e.phone} email={e.email} />
+//     );
+//   });
+//   return allCards;
+// }
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name={contacts[0].name}
+        image={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        image={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        image={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
